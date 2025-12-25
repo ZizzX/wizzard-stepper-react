172422ee163728de44941abd2bc36d4f3e6abeff
ZizzX <66201802+ZizzX@users.noreply.github.com>
2025-12-26 04:01:05 +0500

    build: disable sourcemaps and enable minification

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
     format: ['cjs', 'esm'],
     dts: true,
     splitting: false,
-    sourcemap: true,
+    sourcemap: false,
     clean: true,
-    minify: false, // Better for debugging, user can minify in their app
+    minify: true,
     external: ['react', 'react-dom', 'zod', 'yup'],
 });
